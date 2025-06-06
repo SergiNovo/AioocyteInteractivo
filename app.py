1257216dddfae6c91cd01aae7acf97b62a133755
SergiNovo <sergi.novo@gmail.com>
2025-06-06 09:29:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,15 +43,15 @@ def mostrar_contenido():
                 <div style='font-size: 48px; font-weight: bold; color: #005EA8;'>
                     {dato['Survival']:.1f}%
                 </div>
-                <div style='font-size: 16px; color: #444;'>Probability of Survival</div>
+                <div style='font-size: 16px; color: #444;'>Probability of oocyte survival after vitrification</div>
             </div>
             <hr style="margin: 20px 0;">
         """, unsafe_allow_html=True)
         col1, col2, col3, col4 = st.columns(4)
         col1.metric("Area %", f"{dato['Area%']:.3f}")
-        col2.metric("Circularity", f"{dato['Circularity']:.3f}")
-        col3.metric("V. Deshidratación", f"{dato['Vdeshidratacion']:.2f}%")
-        col4.metric("V. Deplasmolisis", f"{dato['Vdeplasmolisi']:.2f}%")
+        col2.metric("Circularity = 1 indicates a perfect circle", f"{dato['Circularity']:.3f}")
+        col3.metric("Oocyte dehydration rate %/s", f"{dato['Vdeshidratacion']:.2f}%")
+        col4.metric("Oocyte deplasmolysis rate %/s", f"{dato['Vdeplasmolisi']:.2f}%")
 
 # Imagen de fondo del slider + slider en contenedor editable
 with st.container():
